793c2acdf9a79e5454acbcac888f5fbe691bb5b9
Ramilo-Edric03 <edric.ramilo03@gmail.com>
2025-12-03 23:45:34 +0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-    integrations:[react()],
-    vite: {
-        plugins: [tailwindcss()],
-        server: {
-            host: "0.0.0.0",
-            port: 4321,
-            open: true,
-            allowedHosts: ["c7066b399c9d.ngrok-free.app"],
-        },
+  site: 'https://Ramilo-Edric03.github.io',
+  output: 'static',
+
+  integrations: [react()],
+  
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      host: "0.0.0.0",
+      port: 4321,
+      open: true,
+      allowedHosts: ["c7066b399c9d.ngrok-free.app"],
     },
+  },
 });
